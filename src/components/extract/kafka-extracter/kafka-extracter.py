a2924f79b93469b1060da55a9df663934ffe508a
Dordor8 <farfarador@gmail.com>
2026-03-11 22:56:10 +0200

    basic kafka extracter, not working yet

---
+++ src/components/extract/kafka-extracter/kafka-extracter.py
@@ -0,0 +1,23 @@
+import os
+from kafka import KafkaConsumer
+
+topic = os.getenv("TOPIC")
+bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS")
+group_id = os.getenv("GROUP_ID")
+
+output_file = os.getenv("OUTPUT_FILE")
+
+
+consumer = KafkaConsumer(topic,
+                         bootstrap_servers=[bootstrap_servers.split(',')],
+                         group_id=group_id)
+
+def extract():
+    message = consumer.poll(max_records=1)
+
+    with open(output_file, "w") as file:
+        file.write(str(message))
+
+
+if __name__ == "__main__":
+    extract()
