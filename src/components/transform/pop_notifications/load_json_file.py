e421a25e9ac83fd32c309ed1ce5336b444b2509b
Shirel <shireldavidpor@gmail.com>
2026-03-16 19:41:57 +0200

    extract bdt fields from the schema

--- src/components/transform/pop_notifications/load_json_file.py
+++ src/components/transform/pop_notifications/load_json_file.py
@@ -19,12 +19,22 @@ def load_json_file(path, file_name):
 def notify_positive_covid_test(df,)
 
 def extract_bdt(schema):
+    bdt = schema.get("bdt", {})
+    phone = None
+    covid_test = None
+    for col, meaning in bdt.items():
+        if meaning == "PHONE":
+            phone= col
+        if meaning == "COVID_TEST":
+            covid_test= col
+    return phone, covid_test
+
 
 def main():
     load_dotenv(".env")
     folder = os.getenv("FOLDER")
     schema = json.loads(os.getenv('SCHEMA'))
-    
+    extract_bdt(schema)
 
 
 
