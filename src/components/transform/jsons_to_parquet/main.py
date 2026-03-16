b23454d891762e88d3bd38f047c3f447b8769858
Dordor8 <farfarador@gmail.com>
2026-03-16 20:03:18 +0200

    ready for production

--- src/components/transform/jsons_to_parquet/main.py
+++ src/components/transform/jsons_to_parquet/main.py
@@ -1,30 +1,42 @@
 import glob
-import json
-import os.path
 import uuid
 import pyarrow as pa
 import pyarrow.json as pa_json
 import pyarrow.parquet as pa_parquet
 import os
-from dotenv import load_dotenv
-from directory_size import get_dir_size_in_bytes
 
-with open('optimal_size.json', 'r') as file:
-    config = json.load(file)
+
+config = {
+  "minimal_size": 0.256,
+  "maximal_size": 1
+}
 
 max_size = config["maximal_size"]
 min_size = config["minimal_size"]
 
-load_dotenv()
-output_path = os.getenv("output_path")
-folder_path = os.getenv("folder_path")
+output_path = os.getenv("OUTPUT_PATH")
+input_path = os.getenv("INPUT_FOLDER")
+
+
+def get_dir_size_in_bytes(folder_path: str):
+    total = 0
+    try:
+        for entry in os.scandir(input_path):
+            if entry.is_file():
+                total += os.path.getsize(entry.path)
+            elif entry.is_dir():
+                total += get_dir_size_in_bytes(entry.path)
+    except NotADirectoryError:
+        return os.path.getsize(folder_path)
+
+    return total
 
 
 def convert_folder_to_parquet():
     _dirct_folder_conversion()
 
 def _convert_jsons_to_parquet():
-    json_files = glob.glob(os.path.join(folder_path, "*.json"))
+    json_files = glob.glob(os.path.join(input_path, "*.json"))
     tables = []
     for json_file in json_files:
         tables.append(pa_json.read_json(json_file))
@@ -36,7 +48,7 @@ def _convert_jsons_to_parquet():
 
 
 def _dirct_folder_conversion():
-    folder_size = get_dir_size_in_bytes(folder_path) / (1024 ** 3)
+    folder_size = get_dir_size_in_bytes(input_path) / (1024 ** 3)
     if max_size > folder_size > min_size:
         _convert_jsons_to_parquet()
     elif folder_size < min_size:
