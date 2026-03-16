ff63ec5d362fc97d024334693f1088affb367d93
Shirel <shireldavidpor@gmail.com>
2026-03-16 22:40:02 +0200

    added logic, that handles each file in the folder

--- src/components/transform/pop_notifications/load_json_file.py
+++ src/components/transform/pop_notifications/load_json_file.py
@@ -3,7 +3,7 @@ import logging
 import os
 
 import pandas as pd
-from dotenv import load_dotenv
+import requests
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -16,7 +16,30 @@ def load_json_file(path, file_name):
     return df
 
 
-def notify_positive_covid_test(df,)
+def send_notifications(phone, string_message):
+    url = os.getenv("API_URL")
+    payload = {
+        "phone": phone,
+        "message": string_message
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        logging.info(f" notification sent to {phone}")
+    else:
+        logging.warning(f"failed sending to {phone}")
+
+
+def notify_positive_covid_test(df, phone_as_string, covid_test_as_string):
+    if not phone_as_string or not covid_test_as_string:
+        logging.warning("phone or covid test fields ar missing in the BDT")
+        return
+    for _, row in df.iterrows():
+        phone = row.get(phone_as_string)
+        test = row.get(covid_test_as_string)
+
+        if test:
+            send_notifications(phone, "you are positive for Covid19! you must go into isolation immediately!!!!")
+
 
 def extract_bdt(schema):
     bdt = schema.get("bdt", {})
@@ -24,18 +47,21 @@ def extract_bdt(schema):
     covid_test = None
     for col, meaning in bdt.items():
         if meaning == "PHONE":
-            phone= col
+            phone = col
         if meaning == "COVID_TEST":
-            covid_test= col
+            covid_test = col
     return phone, covid_test
 
 
-def main():
-    load_dotenv(".env")
-    folder = os.getenv("FOLDER")
-    schema = json.loads(os.getenv('SCHEMA'))
-    extract_bdt(schema)
+def transform_json_files_in_folder(folder, schema):
+    phone, covid_test = extract_bdt(schema)
+    logging.info(f"phone and covid test result: {phone, covid_test}")
 
+    for file_name in os.listdir(folder):
+        if file_name.endswith(".json"):
+            path = os.path.join(folder, file_name)
+            df = load_json_file(path, file_name)
+            notify_positive_covid_test(df, phone, covid_test)
 
 
 
--- src/components/transform/pop_notifications/notification_basic_api.py
+++ src/components/transform/pop_notifications/notification_basic_api.py
@@ -4,5 +4,14 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Notification(BaseModel):
+    phone: str
+    message: str
+
     
+@app.post("notifications_covid_tests")
+def send_notifications(notification: Notification):
+    print(f"notification sent to the phone {notification.phone}")
+    print(notification.message)
+    return {"status": "success", "message": "notif sent"}
