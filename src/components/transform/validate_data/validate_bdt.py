809a25f7c66a29e02dee86d8cbedb15a212a7faf
Talia <nachumatalia@gmail.com>
2026-03-16 13:56:25 +0200

    corrected bdt validation func, still need to add an invalid bdt handeling and type checks according to schema

--- src/components/transform/validate_data/validate_bdt.py
+++ src/components/transform/validate_data/validate_bdt.py
@@ -1,20 +1,18 @@
-import ast
 import json
-import os
 import uuid
-
-from dotenv import load_dotenv
 from validate_full_name import validate_full_name
 from validate_id import validate_israeli_id
 from validate_phone_number import validate_phone_number
 from validate_address import validate_address
 from validate_covid_result import validate_covid_test_result
 from s3_loader.s3_handler.write_to_s3 import write_to_s3
+from dotenv import load_dotenv
+import os
 
-load_dotenv()
+load_dotenv(override=True)
 json_folder_path = os.getenv("folder_path")
 schema = os.getenv("schema")
-schema_as_dict = ast.literal_eval(schema)
+schema_as_dict = json.loads(schema)
 bdt = schema_as_dict["bdt"]
 s3_prefix = os.getenv("s3_prefix")
 validated_jsons_folder_path = os.getenv("validated_jsons_folder_path")
@@ -22,6 +20,7 @@ validated_jsons_folder_path = os.getenv("validated_jsons_folder_path")
 
 def validate_bdt():
     for filename in os.listdir(json_folder_path):
+        result = True
         with open(os.path.join(json_folder_path, filename)) as f:
             json_data = json.load(f)
         for single_json in json_data:
@@ -35,20 +34,28 @@ def validate_bdt():
                 try:
                     match bdt_value:
                         case "id":
-                            validate_israeli_id(json_bdt_value)
+                            result = validate_israeli_id(json_bdt_value)
                         case "full_name":
-                            validate_full_name(json_bdt_value)
+                            result = validate_full_name(json_bdt_value)
                         case "phone_number":
-                            validate_phone_number(json_bdt_value)
+                            result = validate_phone_number(json_bdt_value)
                         case "address":
-                            validate_address(json_bdt_value)
+                            result = validate_address(json_bdt_value)
                         case "covid_test_result":
-                            validate_covid_test_result(json_bdt_value)
+                            result = validate_covid_test_result(json_bdt_value)
+                    if result is False:
+                        raise ValueError(f"Validation failed for {bdt_value}")
                 except ValueError:
                     error = True
             if error:
+                print("unvalidated")
                 write_to_s3(single_json, s3_prefix)
             else:
+                print("validated")
                 validated_path = os.path.join(validated_jsons_folder_path, f"validated_{uuid.uuid4()}.json")
                 with open(validated_path, "w") as f:
                     json.dump(single_json, f)
+
+
+if __name__ == "__main__":
+    validate_bdt()
