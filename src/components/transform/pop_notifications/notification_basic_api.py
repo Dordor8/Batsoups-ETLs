fe7dfae5aa08db74773ebf118654578a237e5f96
Shirel <shireldavidpor@gmail.com>
2026-03-17 00:06:47 +0200

    API service based pop notifications working, along with test_file as an example

--- src/components/transform/pop_notifications/notification_basic_api.py
+++ src/components/transform/pop_notifications/notification_basic_api.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 import pydantic
 from pydantic import BaseModel
@@ -9,9 +10,13 @@ class Notification(BaseModel):
     phone: str
     message: str
 
-    
-@app.post("notifications_covid_tests")
+
+@app.post("/notifications_covid_tests")
 def send_notifications(notification: Notification):
     print(f"notification sent to the phone {notification.phone}")
     print(notification.message)
-    return {"status": "success", "message": "notif sent"}
+    return {"status": "success- notification sent!!", "phone": notification.phone, "message": notification.message}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
